ace03ed9aed353a840b4403862dbe3602c220dd6
yjson <handyejee14@gmail.com>
2024-12-09 21:09:01 +0900

    Initial commit

---
+++ com.sparta.logistics.notification/src/test/java/com/sparta/logistics/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.logistics.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
