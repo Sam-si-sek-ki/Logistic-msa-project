6d1fa8dd1622dd3ce653be979f8c21c55a4d8ebf
yjson <handyejee14@gmail.com>
2024-12-15 01:31:51 +0900

    fix : feign client interface 로 받도록 수정

--- com.sparta.logistics.delivery/src/main/java/com/sparta/logistics/delivery/infrastructure/client/OrderServiceClient.java
+++ com.sparta.logistics.delivery/src/main/java/com/sparta/logistics/delivery/infrastructure/client/OrderServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "order-service")
-public class OrderServiceClient {
+public interface OrderServiceClient {
 
 //  @GetMapping("/orders/{orderId}")
 //  OrderResponse getOrder(@PathVariable UUID orderId);
