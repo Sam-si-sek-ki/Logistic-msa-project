9cdf0afa29256cc0f60705af9905c6726547828e
yoaruku <rhrudgh12@naver.com>
2024-12-16 01:30:15 +0900

    Feat: 허브 목록조회 구현 Close: #27

--- com.sparta.logistics.hub/src/main/java/com/sparta/logistics/hub/domain/repository/HubRepository.java
+++ com.sparta.logistics.hub/src/main/java/com/sparta/logistics/hub/domain/repository/HubRepository.java
@@ -1,9 +1,37 @@
 package com.sparta.logistics.hub.domain.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.StringPath;
 import com.sparta.logistics.hub.domain.model.Hub;
+import com.sparta.logistics.hub.domain.model.QHub;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
-public interface HubRepository extends JpaRepository<Hub, String>, QuerydslPredicateExecutor<Hub> {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
+public interface HubRepository extends JpaRepository<Hub, String>,
+        QuerydslPredicateExecutor<Hub>,
+        QuerydslBinderCustomizer<QHub> {
+
+
+    @Override
+    default void customize(QuerydslBindings bindings, @NotNull QHub qHub) {
+        bindings.bind(String.class).all((StringPath path, Collection<? extends String> values) -> {
+            List<String> valueList = new ArrayList<>(values.stream().map(String::trim).toList());
+            if (valueList.isEmpty()) {
+                return Optional.empty();
+            }
+            BooleanBuilder bb = new BooleanBuilder();
+            for (String str : valueList) {
+                bb.or(path.containsIgnoreCase(str));
+            }
+            return Optional.of(bb);
+        });
+    }
 }
