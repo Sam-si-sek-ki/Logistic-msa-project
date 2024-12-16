0dcf7b696ec3af4260757cf48a8e20e0dbd78ef0
Haena <gogohaena@gmail.com>
2024-12-17 00:45:46 +0900

    Feat: AuditingConfig, QueryDslConfig 추가
    - AuditingConfig 추가
    - QueryDslConfig 추가
    - User 쿼리 isDeleted 조건 추가

--- com.sparta.logistics.auth/src/main/java/com/sparta/logistics/auth/domain/repository/UserRepository.java
+++ com.sparta.logistics.auth/src/main/java/com/sparta/logistics/auth/domain/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.sparta.logistics.auth.domain.repository;
 
 import com.sparta.logistics.auth.domain.model.User;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,7 +10,9 @@ public interface UserRepository {
 
     User save(User user);
 
+    @Query("SELECT u FROM User u WHERE u.username = :username AND u.isDeleted = false")
     Optional<User> findByUsername(String username);
 
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.isDeleted = false")
     Optional<User> findByEmail(String email);
 }
