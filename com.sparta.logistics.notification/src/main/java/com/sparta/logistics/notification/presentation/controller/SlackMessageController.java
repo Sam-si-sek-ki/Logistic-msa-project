035cea4ef0f008a331e9fa3763092d7ce45c47be
Haena <gogohaena@gmail.com>
2024-12-17 20:54:43 +0900

    Feat: Notification - Swagger 적용

--- com.sparta.logistics.notification/src/main/java/com/sparta/logistics/notification/presentation/controller/SlackMessageController.java
+++ com.sparta.logistics.notification/src/main/java/com/sparta/logistics/notification/presentation/controller/SlackMessageController.java
@@ -6,6 +6,8 @@ import com.sparta.logistics.notification.libs.model.SuccessResponse;
 import com.sparta.logistics.notification.presentation.dto.SendSlackMessageRequest;
 import com.sparta.logistics.notification.presentation.dto.SendSlackMessageResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,12 @@ public class SlackMessageController {
 
     @PostMapping("/slack/send")
     @Operation(summary = "슬랙 메시지 전송", description = "슬랙 메시지 전송 요청 API")
+    @Parameters({
+            @Parameter(name = "recipientId", description = "슬랙 수신자 ID", example = "U085GB6CSRJ", required = true),
+            @Parameter(name = "message", description = "보낼 메시지 내용", example = "This is a test Slack message.", required = true),
+            @Parameter(name = "serviceName", description = "요청 서비스명", example = "DELIVERY"),
+            @Parameter(name = "referenceId", description = "요청 서비스의 참조 엔티티ID", example = "b8b06bde-3e4d-4e16-9c13-2d23e92bb63d")
+    })
     public ResponseEntity<SuccessResponse<SendSlackMessageResponse>> sendSlackMessage(
             @RequestBody @Valid SendSlackMessageRequest request) {
         return ResponseEntity.ok().body(
