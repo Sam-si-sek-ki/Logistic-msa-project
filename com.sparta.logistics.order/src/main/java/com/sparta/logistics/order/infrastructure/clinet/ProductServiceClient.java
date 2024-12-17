ca2679ab25df1af9cc7e0fe4769ad64523f1af4d
geonuu09 <rjsdndl909@gmail.com>
2024-12-17 12:42:40 +0900

    comment : todo 제거

--- com.sparta.logistics.order/src/main/java/com/sparta/logistics/order/infrastructure/clinet/ProductServiceClient.java
+++ com.sparta.logistics.order/src/main/java/com/sparta/logistics/order/infrastructure/clinet/ProductServiceClient.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "product-service")
 public interface ProductServiceClient {
